9a51cebf1d9867165619e99b87bc6c5362c7ea36
Paul Treadwell <paultreadwell@pauls-imac.local>
2016-10-16 13:02:48 +0100

    Basic interactions catered for, not efficient, no modularisation. Changes to be done in next sprint: Set up constraints programmatically (labels used in viewcontroller class, apples in draggable:UIImageView class) | Bring in segue for win screen and navigation back to main screen |Modularise answer button click (change to image rather than button, create a sub class of the UIImage class to handle behaviours for this object, attach class to all image objects that need to be clicked to give an answer) | Set up more constraints programmatically (clickable buttons in new class created)

--- [15070779]CW1_Calculator_App/ViewController.swift
+++ [15070779]CW1_Calculator_App/ViewController.swift
@@ -8,11 +8,123 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblNumber1: UILabel!
+    @IBOutlet weak var lblNumber2: UILabel!
+    var intTotal = 10
+    
+    @IBOutlet weak var lblWellDone: UILabel!
+    
+    @IBAction func btnReload(_ sender: AnyObject) {
+        
+        viewDidLoad()
+    }
+    @IBAction func tuButton0(_ sender: AnyObject) {
+        
+        if (intTotal == 0) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+      
+    }
+    
+    @IBAction func tuButton1(_ sender: AnyObject) {
+        if (intTotal == 1) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton2(_ sender: AnyObject) {
+        if (intTotal == 2) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton3(_ sender: AnyObject) {
+        if (intTotal == 3) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton4(_ sender: AnyObject) {
+        if (intTotal == 4) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton5(_ sender: AnyObject) {
+        if (intTotal == 5) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton6(_ sender: AnyObject) {
+        if (intTotal == 6) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    @IBAction func tuButton7(_ sender: AnyObject) {
+        if (intTotal == 7) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    
+    @IBAction func tuButton8(_ sender: AnyObject) {
+        if (intTotal == 8) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+    
+    @IBAction func tuButton9(_ sender: AnyObject) {
+        if (intTotal == 9) {
+            
+            lblWellDone.isHidden = false
+            
+        }
+    }
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblWellDone.isHidden = true
+        
+        let intNumber1: Int = Int(arc4random_uniform(4) + 1)
+        let intNumber2: Int = Int(arc4random_uniform(4) + 1)
+        intTotal = intNumber1 + intNumber2
+        
+
+        
+        lblNumber1.text = String(intNumber1)
+        lblNumber2.text = String(intNumber2)
     }
 
     override func didReceiveMemoryWarning() {
